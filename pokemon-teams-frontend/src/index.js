4680e77298b37d4fd570ff96a954c14b1298ff05
Babyyodabc <rjwahba@gmail.com>
2020-03-18 11:14:29 -0400

    wip

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -18,40 +18,37 @@ function renderTrainers(trainer) {
     console.log(trainer.pokemons)
     const mainCon = document.getElementsByTagName("main")[0]
     const divCard = document.createElement("div")
+    const ul = document.createElement("ul")
+    const tPokes = trainer.pokemons
     divCard.dataset.id = trainer.id
     divCard.innerHTML = `
     <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
     <button data-trainer-id="${trainer.id}">Add Pokemon</button>
-  <ul>
-//     <li>${trainer.pokemons[0].nickname}  (${trainer.pokemons[0].species}) <button class="release" data-pokemon-id="${trainer.pokemons[0].id}">Release</button></li>
-//     <li>${trainer.pokemons[1].nickname}  (${trainer.pokemons[1].species}) <button class="release" data-pokemon-id="${trainer.pokemons[1].id}">Release</button></li>
-//     <li>${trainer.pokemons[2].nickname}  (${trainer.pokemons[2].species}) <button class="release" data-pokemon-id="${trainer.pokemons[2].id}">Release</button></li>
-//     <li>${trainer.pokemons[3].nickname}  (${trainer.pokemons[3].species}) <button class="release" data-pokemon-id="${trainer.pokemons[3].id}">Release</button></li>
-//     <li>${trainer.pokemons[4].nickname}  (${trainer.pokemons[4].species}) <button class="release" data-pokemon-id="${trainer.pokemons[4].id}">Release</button></li>
-   </ul>
-</div>
+  
+    </div>
     `
-    mainCon.appendChild(divCard)
-
-}
-
-function renderPokemon(trainer) {
-    const tPokes = trainer.pokemons
     tPokes.forEach( pokemon => {
-        const ul = documemt.createElement("ul")
-
+        ul.innerHTML = `
+        <li>${pokemon.nickname}  (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>
+        <li>${pokemon.nickname}  (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>
+        <li>${pokemon.nickname}  (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>
+        <li>${pokemon.nickname}  (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>
+        <li>${pokemon.nickname}  (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>
+        `
     })
-
+    divCard.append(ul)
+    mainCon.appendChild(divCard)
+    
 }
 
 
 
-function addPokemon() {
-    document.addEventListener("click", function(event){
-        event.preventDefault()
-        if (event.target.innerText === "Add Pokemon")
-            let new
 
-    })
+// function addPokemon() {
+//     document.addEventListener("click", function(event){
+//         event.preventDefault()
+//         if (event.target.innerText === "Add Pokemon")
+//             let new
 
-}
+//     })
+
